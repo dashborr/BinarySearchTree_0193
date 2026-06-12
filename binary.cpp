44f90efaae1e80041267349ccfce31e6281fcf26
dashborr <danisarfas7@gmail.com>
2026-06-12 17:03:46 +0700

    menambahkan if di void inorder

--- binary.cpp
+++ binary.cpp
@@ -58,4 +58,14 @@ void search(string element, Node*& parent, Node*& currentNode) {
     }
 }
 
-void inorder(Node* ptr)
+void inorder(Node* ptr){
+    if (ptr != nullptr) {
+        cout << "tree is empty" << endl;
+        return;
+    }
+    if (ptr != nullptr) {
+        inorder(ptr->leftchild);
+        cout << ptr->info << " ";
+        inorder(ptr->rightchild);
+    }
+}
